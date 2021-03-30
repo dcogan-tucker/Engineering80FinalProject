b10f354ca3b76802c23f2123ae45b855ee37f45f
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-30 11:09:00 +0100

    Initial commit

---
+++ src/main/java/com/sparta/eng80/spartaonetooneformtracker/SpartaOneToOneFormTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.eng80.spartaonetooneformtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaOneToOneFormTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaOneToOneFormTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/eng80/spartaonetooneformtracker/SpartaOneToOneFormTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.eng80.spartaonetooneformtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaOneToOneFormTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
