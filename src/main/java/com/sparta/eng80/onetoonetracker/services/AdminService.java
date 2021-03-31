e18216f43a41a6bfd96b7be3864d78bb7589f46f
Xavier2a2 <abanjoko@spartaglobal.com>
2021-03-31 16:36:56 +0100

    Merge branch 'feature-adminService+adminServiceTests' of /Users/aaronbanjoko/Documents/Sparta-Files/Engineering80FinalProject with conflicts.

--- src/main/java/com/sparta/eng80/onetoonetracker/services/AdminService.java
+++ src/main/java/com/sparta/eng80/onetoonetracker/services/AdminService.java
@@ -1,18 +1,41 @@
 package com.sparta.eng80.onetoonetracker.services;
 
 import com.sparta.eng80.onetoonetracker.entities.TrainerEntity;
+import com.sparta.eng80.onetoonetracker.repositories.TrainerRepository;
 import com.sparta.eng80.onetoonetracker.services.interfaces.AdminAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService implements AdminAppService {
-    @Override
-    public TrainerEntity saveTrainer(TrainerEntity trainer) {
-        return null;
+
+    private final TrainerRepository trainerRepository;
+
+    public AdminService(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
     }
 
     @Override
-    public TrainerEntity deleteTrainer(TrainerEntity trainer) {
-        return null;
+    public TrainerEntity saveTrainer(TrainerEntity trainer) {
+        return trainerRepository.save(trainer);
+    }
+
+    @Override
+    public void deleteTrainer(TrainerEntity trainer) {
+        trainerRepository.delete(trainer);
+    }
+
+    public void deleteTrainerById(int trainerId) {
+        trainerRepository.deleteTrainerById(trainerId);
+    }
+
+    @Override
+    public TrainerEntity findTrainerById(int trainerId) {
+        return trainerRepository.findTrainerById(trainerId);
+    }
+
+    @Override
+    public Iterable<TrainerEntity> findAllTrainers(){
+        return trainerRepository.findAll();
     }
 }
--- src/main/java/com/sparta/eng80/onetoonetracker/services/interfaces/AdminAppService.java
+++ src/main/java/com/sparta/eng80/onetoonetracker/services/interfaces/AdminAppService.java
@@ -5,5 +5,7 @@ import com.sparta.eng80.onetoonetracker.entities.TrainerEntity;
 public interface AdminAppService {
 
     TrainerEntity saveTrainer(TrainerEntity trainer);
-    TrainerEntity deleteTrainer(TrainerEntity trainer);
+    void deleteTrainer(TrainerEntity trainer);
+    TrainerEntity findTrainerById(int trainerId);
+    Iterable<TrainerEntity> findAllTrainers();
 }
