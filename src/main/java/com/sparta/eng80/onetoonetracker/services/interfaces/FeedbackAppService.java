984372f521ada2275af67afd19d50da33d6ad26a
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-31 16:27:56 +0100

    added java doc to service interfaces

--- src/main/java/com/sparta/eng80/onetoonetracker/services/interfaces/FeedbackAppService.java
+++ src/main/java/com/sparta/eng80/onetoonetracker/services/interfaces/FeedbackAppService.java
@@ -3,8 +3,24 @@ package com.sparta.eng80.onetoonetracker.services.interfaces;
 import com.sparta.eng80.onetoonetracker.entities.FeedbackEntity;
 import com.sparta.eng80.onetoonetracker.entities.TrainerEntity;
 
+/**
+ * Interface to be implemented by a service dealing with feedback entities.
+ */
 public interface FeedbackAppService extends ApplicationService<FeedbackEntity> {
 
+    /**
+     * Finds all feedback for the given trainee from the repository.
+     *
+     * @param trainee The trainee to find the feedback for.
+     * @return An Iterable containing all the feedback for a given trainee.
+     */
     Iterable<FeedbackEntity> findByTrainee(TrainerEntity trainee);
+
+    /**
+     * Finds all feedback for the given trainer from the repository.
+     *
+     * @param trainer The trainer to find the feedback for.
+     * @return An Iterable containing all the feedback for a given trainer.
+     */
     Iterable<FeedbackEntity> findByTrainer(TrainerEntity trainer);
 }
