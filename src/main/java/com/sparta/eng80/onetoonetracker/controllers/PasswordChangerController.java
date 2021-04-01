4b0a36460e8f300b17604749fa7dc21c48c6947a
WahdelMW <WWoodhouse@spartaglobal.com>
2021-04-01 11:31:46 +0100

    Passed and handled password inputs in controller

--- src/main/java/com/sparta/eng80/onetoonetracker/controllers/PasswordChangerController.java
+++ src/main/java/com/sparta/eng80/onetoonetracker/controllers/PasswordChangerController.java
@@ -1,9 +1,12 @@
 package com.sparta.eng80.onetoonetracker.controllers;
 
 import com.sparta.eng80.onetoonetracker.entities.UserEntity;
+import com.sparta.eng80.onetoonetracker.security.PasswordEncryptor;
 import com.sparta.eng80.onetoonetracker.services.SecurityService;
 import com.sparta.eng80.onetoonetracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,18 +26,33 @@ public class PasswordChangerController {
         this.securityService = securityService;
     }
 
-    @GetMapping("/changePassword")
+    @GetMapping("/change-password")
     public String changePassword(Model model) {
         UserEntity user = securityService.getCurrentUser();
-        model.addAttribute("user", user);
-        //Change to name of password changer page
-        return "changePassword";
+        return "change_password";
     }
 
-    @PostMapping("/changePassword")
-    public void setPassword(@ModelAttribute("user") UserEntity userEntity, @RequestParam("password") String password) {
-        userEntity.setPassword(password);
-        userEntity.setPasswordChanged(true);
-        userService.save(userEntity);
+    @PostMapping("/change-password")
+    public String setPassword(
+                            @RequestParam("currentPassword") String currentPassword,
+                            @RequestParam("newPassword") String newPassword,
+                            @RequestParam("confirmPassword") String confirmPassword) {
+        UserEntity userEntity = securityService.getCurrentUser();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.matches(userEntity.getPassword(), currentPassword));
+        System.out.println(newPassword.equals(confirmPassword));
+        System.out.println("P: " + currentPassword);
+        System.out.println("NP: " + newPassword);
+        System.out.println("CP: " + confirmPassword);
+        if (!newPassword.equals(confirmPassword)) {
+            System.out.println("Error");
+            return "redirect:/change-password";
+        } else {
+            System.out.println("Changed");
+            return "index";
+//            userEntity.setPassword(newPassword);
+//            userEntity.setPasswordChanged(true);
+//            userService.save(userEntity);
+        }
     }
 }
