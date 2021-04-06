e977fe0577386bfecc7ff4c5207016f03857ade4
WahdelMW <WWoodhouse@spartaglobal.com>
2021-04-06 16:36:10 +0100

    Added test to check correct page is loaded for '/login' mapping

--- src/test/java/com/sparta/eng80/onetoonetracker/controllers/LoginControllerTest.java
+++ src/test/java/com/sparta/eng80/onetoonetracker/controllers/LoginControllerTest.java
@@ -4,16 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginControllerTest {
 
     @Autowired
     private LoginController loginController;
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
     @Test
     public void loadsContext() {
         Assertions.assertNotNull(loginController);
+    }
+
+    @Test
+    public void checkPageReturn() {
+        Assertions.assertTrue(this.testRestTemplate.getForObject("http://localhost:" + port + "/", String.class).contains("Login"));
     }
 }
