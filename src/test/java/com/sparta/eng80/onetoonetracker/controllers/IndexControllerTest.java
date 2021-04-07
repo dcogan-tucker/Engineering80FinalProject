bcab945f15162725cdfdf6c2ef2e9354b3ca1837
WahdelMW <WWoodhouse@spartaglobal.com>
2021-04-07 09:51:33 +0100

    Commit so pull can be done

--- src/test/java/com/sparta/eng80/onetoonetracker/controllers/IndexControllerTest.java
+++ src/test/java/com/sparta/eng80/onetoonetracker/controllers/IndexControllerTest.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
 
     @Autowired
     private IndexController indexController;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
 
     @Test
     public void loadsContext() {
